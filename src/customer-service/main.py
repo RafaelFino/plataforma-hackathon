c931fd5afaa8fc98f0e53cda5e9737b00bb4eacb
Rafael Gottardi (Fino) <rafael.gottardi@gmail.com>
2025-01-21 00:17:07 -0300

    First

--- src/customer-service/main.py
+++ src/customer-service/main.py
@@ -3,7 +3,7 @@
 import logging
 from http import HTTPStatus
 from datetime import datetime
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Response
 
 from domain.customer import CustomerDomain
 from service.customer import CustomerService
@@ -26,7 +26,7 @@ logging.basicConfig(
     datefmt='%H:%M:%S',
 )
 
-def makeResponse(msg: str, args: dict = {}):
+def makeResponse(response: Response, msg: str, args: dict = {}, status: HTTPStatus = HTTPStatus.OK ) -> dict:
     ret = {
         "message": msg,
         "timestamp": datetime.now().isoformat()
@@ -34,6 +34,8 @@ def makeResponse(msg: str, args: dict = {}):
 
     for key in args:
         ret[key] = args[key]
+
+    response.status_code = status
 
     return ret
 
@@ -48,16 +50,20 @@ def check_id(id):
         
     return False
 
+@app.get('/')
+def root(response: Response):
+    return makeResponse(response, "Customer API", status=HTTPStatus.OK)
+
 @app.post('/customer')
-def add_customer():
+def add_customer(response: Response):
     data = Request.get_json()
     customer = CustomerDomain(data['name'], data['email'])
     customer_id = service.add(customer)
 
-    return makeResponse("Customer added", {"customer_id": customer_id}), HTTPStatus.CREATED
+    return makeResponse(response, "Customer added", {"customer_id": customer_id }, status=HTTPStatus.CREATED)
 
 @app.put('/customer/{id}')
-def update_customer(id):
+def update_customer(id, response: Response):
     if check_id(id) == False:
         return makeResponse("Invalid ID"), HTTPStatus.BAD_REQUEST
 
@@ -67,40 +73,40 @@ def update_customer(id):
     updated = service.update(customer)
 
     if updated:
-        return makeResponse("Customer updated"), HTTPStatus.OK
+        return makeResponse(response, "Customer updated", status=HTTPStatus.OK)
     else:
-        return makeResponse("Customer not found"), HTTPStatus.NO_CONTENT
+        return makeResponse(response, "Customer not found", status=HTTPStatus.NO_CONTENT)
     
 @app.delete('/customer/{id}')
-def delete_customer(id):
+def delete_customer(id, response: Response):
     if check_id(id) == False:
-        return makeResponse("Invalid ID"), HTTPStatus.BAD_REQUEST
+        return makeResponse(response, "Invalid ID", status=HTTPStatus.BAD_REQUEST)
 
     deleted = service.delete(id)
 
     if deleted:
-        return makeResponse("Customer deleted"), HTTPStatus.OK
+        return makeResponse(response, "Customer deleted", status=HTTPStatus.OK)
     else:
-        return makeResponse("Customer not found"), HTTPStatus.NO_CONTENT
+        return makeResponse(response, "Customer not found", status=HTTPStatus.NO_CONTENT)
     
 @app.get('/customer/{id}')
-def get_customer(id):
+def get_customer(id, response: Response):
     if check_id(id) == False:
-        return makeResponse("Invalid ID"), HTTPStatus.BAD_REQUEST
+        return makeResponse(response, "Invalid ID", status=HTTPStatus.BAD_REQUEST)
         
     customer = service.get(id)
 
     if customer:
-        return makeResponse("Customer found", customer.to_json()), HTTPStatus.OK
+        return makeResponse(response, "Customer found", customer.to_json(), status=HTTPStatus.OK)
     else:
-        return makeResponse("Customer not found"), HTTPStatus.NO_CONTENT
+        return makeResponse(response, "Customer not found", status=HTTPStatus.NO_CONTENT)
     
 @app.get('/customer')
-def get_all_customers():
+def get_all_customers(response: Response):
     customers = service.get_all()
     customers_json = [customer.to_json() for customer in customers]
 
-    return makeResponse("Customers found", {"customers": customers_json}), HTTPStatus.OK
+    return makeResponse(response, "Customers found", {"customers": customers_json}, status=HTTPStatus.OK)
 
 print("Done!")
 
--- src/product-service/main.py
+++ src/product-service/main.py
@@ -3,7 +3,7 @@
 import logging
 from http import HTTPStatus
 from datetime import datetime
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Response
 
 from domain.product import ProductDomain
 from service.product import ProductService
@@ -26,7 +26,7 @@ logging.basicConfig(
     datefmt='%H:%M:%S',
 )
 
-def makeResponse(msg: str, args: dict = {}):
+def makeResponse(response: Response, msg: str, args: dict = {}, status: HTTPStatus = HTTPStatus.OK ) -> dict:
     ret = {
         "message": msg,
         "timestamp": datetime.now().isoformat()
@@ -34,6 +34,8 @@ def makeResponse(msg: str, args: dict = {}):
 
     for key in args:
         ret[key] = args[key]
+
+    response.status_code = status
 
     return ret
 
@@ -44,23 +46,26 @@ def check_id(id):
             return True
         
     except ValueError:
-        logging.error(f"[CUSTOMER-API] Invalid ID: {id}")
+        logging.error(f"[PRODUCT-API] Invalid ID: {id}")
         
     return False
 
+@app.get('/')
+def root(response: Response):
+    return makeResponse(response, "Product API", status=HTTPStatus.OK)
 
 @app.post('/product')
-def add_product():
+def add_product(response: Response):
     data = Request.get_json()
     product = ProductDomain(data['name'], data['price'])
     product_id = service.add(product)
 
-    return makeResponse("Product added", {"product_id": product_id}), HTTPStatus.CREATED
+    return makeResponse(response, "Product added", {"product_id": product_id}, status=HTTPStatus.CREATED)
 
 @app.put('/product/{id}')
-def update_product(id):
+def update_product(id, response: Response):
     if check_id(id) == False:
-        return makeResponse("Invalid ID"), HTTPStatus.BAD_REQUEST
+        return makeResponse(response, "Invalid ID", status=HTTPStatus.BAD_REQUEST)
 
     data = Request.get_json()
     product = ProductDomain(data['name'], data['price'])
@@ -68,41 +73,41 @@ def update_product(id):
     updated = service.update(product)
 
     if updated:
-        return makeResponse("Product updated"), HTTPStatus.OK
+        return makeResponse(response, "Product updated", status=HTTPStatus.OK)
     else:
-        return makeResponse("Product not found"), HTTPStatus.NO_CONTENT
+        return makeResponse(response, "Product not found", status=HTTPStatus.NO_CONTENT)
     
 @app.delete('/product/{id}')
-def delete_product(id):
+def delete_product(id, response: Response):
     if check_id(id) == False:
-        return makeResponse("Invalid ID"), HTTPStatus.BAD_REQUEST
+        return makeResponse(response, "Invalid ID", status=HTTPStatus.BAD_REQUEST)
 
     deleted = service.delete(id)
 
     if deleted:
-        return makeResponse("Product deleted"), HTTPStatus.OK
+        return makeResponse(response, "Product deleted", status=HTTPStatus.OK)
     else:
-        return makeResponse("Product not found"), HTTPStatus.NO_CONTENT
+        return makeResponse(response, "Product not found", status=HTTPStatus.NO_CONTENT)
     
 @app.get('/product/{id}')
-def get_product(id):
+def get_product(id, response: Response):
     if check_id(id) == False:
-        return makeResponse("Invalid ID"), HTTPStatus.BAD_REQUEST
+        return makeResponse(response, "Invalid ID", status=HTTPStatus.BAD_REQUEST)
 
     product = service.get(id)
 
     if product is not None:
-        return makeResponse("Product found", {"product": product.to_dict()}), HTTPStatus.OK
+        return makeResponse(response, "Product found", {"product": product.to_dict()}, status=HTTPStatus.OK)
     else:
-        return makeResponse("Product not found"), HTTPStatus.NO_CONTENT
+        return makeResponse(response, "Product not found", status=HTTPStatus.NO_CONTENT)
     
 @app.get('/product')
-def get_all_products():
+def get_all_products(response: Response):
     products = service.get_all()
     ret = []
     for product in products:
         ret.append(product.to_dict())
 
-    return makeResponse("Products found", {"products": ret}), HTTPStatus.OK
+    return makeResponse(response, "Products found", {"products": ret}, status=HTTPStatus.OK)
 
 print("Done!")
